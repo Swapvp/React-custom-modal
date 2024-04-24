9112f82eb5559b186c3306759ad81d41e03603af
Swapvp <swapneshpawar1994@gmail.com>
2024-04-24 12:45:10 +0530

    custom modal

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Modal from "./Components/Modal";
+import ModalTest from "./Components/ModalTest";
+
+function App() {
+  return (
+    <>
+      <ModalTest></ModalTest>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/ModalTest.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import "./Modal.css";
+
+export default function ModalTest() {
+  const [showModalPopup, setShowModalPopup] = useState(false);
+
+  function handleToggleModalPopup() {
+    setShowModalPopup(!showModalPopup);
+  }
+
+  function onClose() {
+    setShowModalPopup(false);
+  }
+
+  return (
+    <>
+      <button className="modalTestBtn" onClick={handleToggleModalPopup}>
+        Open Modal Popup
+      </button>
+      {showModalPopup && (
+        <Modal
+          id={"custom-id"}
+          header={<h1>Customized Header</h1>}
+          footer={<h1>Customized Footer</h1>}
+          onClose={onClose}
+          body={<div>Customized body</div>}
+        />
+      )}
+    </>
+  );
+}
